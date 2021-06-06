94411d43379393115704f0a160282853788713c2
Paulo Humberto <phumbertodasilva@gmail.com>
2021-06-06 12:28:25 -0300

    Finalizando Desafio 02

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import {MovieCard} from "./MovieCard";
+
+import '../styles/content.scss'
+
+interface ContentProps {
+  selectedGenre: { title: string };
+  movies: Array<{
+    imdbID: string;
+    Title: string;
+    Poster: string;
+    Ratings: Array<{
+      Source: string;
+      Value: string;
+    }>;
+    Runtime: string;
+  }>
+}
+
+export function Content({movies, selectedGenre}: ContentProps) {
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime}
+                       rating={movie.Ratings[0].Value}/>
+          ))}
+        </div>
+      </main>
+    </div>
+  );
 }
